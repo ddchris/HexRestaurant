815594bae0e0bf6c05c8810df77fb4e620a99cc9
chris2012 <evilkillerice@gmail.com>
2018-02-08 10:55:29 +0800

    update all.js

--- js/all.js
+++ js/all.js
@@ -20,14 +20,4 @@ $(document).ready(function () {
     $('#bannerText').mouseleave(function () {
         $(this).removeClass('animated tada');
     })
-
-
-
-    // let bannerText = document.getElementById('bannerText');
-    // bannerText.addEventListener('mouseover', function (e) {
-    //     bannerText.setAttribute('class', 'bannerText animated tada  infinite');
-    // })
-    // bannerText.addEventListener('mouseout', function (e) {
-    //     bannerText.setAttribute('class', 'bannerText');
-    // })
 });
